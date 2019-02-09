c87fdea83a4d33dfecf658eba0bfd6ab36d00296
Andrew <46454105+andrewbolek@users.noreply.github.com>
2019-02-08 21:18:09 -0500

    Add files via upload

---
+++ HouseOfCoffee/HouseOfCoffee/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary;
+
+namespace HouseOfCoffee
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            Switch();
+        }
+        static void Switch()
+        {
+
+
+            Console.Clear();
+            int result;
+            string Time =DateTime.Now.ToString("h:mm:ss tt");
+            
+            Console.WriteLine($"                                                 Coffee Shop           {Time}    \n\n\n\n");
+            Console.WriteLine("Press [1] for Total Sales\n\nPress [2] for employees Pay\n\nPress [3] to exit\n");
+            int.TryParse(Console.ReadLine(), out result);
+            switch (result)
+            {
+                case (int)Menu.Calculate:
+                    Sales();
+                    break;
+                case (int)Menu.Pay:
+
+                    Pay();
+                    break;
+                case (int)Menu.Exit:
+                    Exit();
+                    break;
+                default:
+
+
+                    Switch();
+                    break;
+                
+            }
+            Console.ReadLine();
+        }
+        static void Sales()
+        {
+
+
+            string Time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.Clear();
+            Console.WriteLine($"                                                 Sales                 {Time}\n\n\n\n");
+            
+         
+            Coffee coffee = new Coffee();
+            Total ttl = new Total();
+            try
+            {
+                Console.WriteLine("Please enter the amount of Small coffees sold.");
+                
+                int Small = Int32.Parse(Console.ReadLine());
+                ttl.CalculateCTotals(Small,coffee.CoffeePrice, Sizes.Small, coffee.CoffeeCost );
+               
+
+                 Console.WriteLine("Please enter the amount of Medium coffees sold.");
+
+                int Medium = Int32.Parse(Console.ReadLine());
+                ttl.CalculateCTotals(Medium, coffee.CoffeePrice, Sizes.Medium, coffee.CoffeeCost);
+          
+                Console.WriteLine("Please enter the amount of Large coffees sold.");
+
+                int Large = Int32.Parse(Console.ReadLine());
+                ttl.CalculateCTotals(Large, coffee.CoffeePrice, Sizes.Large, coffee.CoffeeCost);
+
+                Console.WriteLine("Please enter the amount of egg sandwiches sold that day");
+                int Egg = Int32.Parse(Console.ReadLine());
+                ttl.CalculateFTotals(Egg, coffee.FoodPrice, Food.Egg, coffee.FoodCost);
+
+
+                Console.WriteLine("Please enter the amount of chicken biscuits sold that day");
+                int Chicken = Int32.Parse(Console.ReadLine());
+                ttl.CalculateFTotals(Chicken, coffee.FoodPrice, Food.Chicken, coffee.FoodCost);
+
+                Console.WriteLine($"Your total Profit is...\n ");
+                Task.Delay(1000).Wait();
+                Console.WriteLine($"                              ${ Total.totalCoffeeSales + Total.totalFoodSales - Total.totalFoodCosts - Total.totalCoffeeCostOfGoods}");
+                Console.ReadLine();
+                Switch();
+            }
+            catch
+            {
+                Console.WriteLine("error");
+                Sales();
+            }
+            
+
+
+
+
+
+
+
+
+
+        }
+
+        static void Pay()
+        {
+            Coffee coffee = new Coffee();
+            Total ttl = new Total();
+
+            string Time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.Clear();
+            Console.WriteLine($"                                                 Pay                   {Time}\n\n\n\n");
+            
+            Console.WriteLine("Please enter your first employees hours\n");
+
+            int Emp1 = Int32.Parse(Console.ReadLine());
+            double Emp1Pay = ttl.CalculatePay(Emp1, coffee.HourPay, MyClassLibrary.Pay.hourly, coffee.Overtime, coffee.OTDiff);
+            
+            
+            Console.WriteLine("\nPlease enter your second employees hours\n");
+
+            int Emp2 = Int32.Parse(Console.ReadLine());
+            double Emp2Pay = ttl.CalculatePay(Emp2, coffee.HourPay, MyClassLibrary.Pay.hourly, coffee.Overtime, coffee.OTDiff);
+            
+            Console.WriteLine("\nPlease enter your third employees hours\n");
+
+            int Emp3 = Int32.Parse(Console.ReadLine());
+            double Emp3Pay = ttl.CalculatePay(Emp3, coffee.HourPay, MyClassLibrary.Pay.hourly, coffee.Overtime, coffee.OTDiff);
+            
+            Console.WriteLine("\nPlease enter your fourth employees hours\n");
+
+            int Emp4 = Int32.Parse(Console.ReadLine());
+            double Emp4Pay = ttl.CalculatePay(Emp4, coffee.HourPay, MyClassLibrary.Pay.hourly, coffee.Overtime, coffee.OTDiff);
+            
+            Console.WriteLine("\nPlease enter your fifth employees hours\n");
+
+            int Emp5 = Int32.Parse(Console.ReadLine());
+            double Emp5Pay = ttl.CalculatePay(Emp5, coffee.HourPay, MyClassLibrary.Pay.hourly, coffee.Overtime, coffee.OTDiff);
+           
+
+            Console.Clear();
+            Console.WriteLine($"                                                 Pay                   {Time}\n\n\n\n");
+            Console.WriteLine($"Your employees weekly pay comes out to...");
+            Task.Delay(1000).Wait();
+            Console.WriteLine(
+
+                $"\n Employee 1 = ${Emp1Pay} " +
+
+                $"\n Employee 2 = ${Emp2Pay}" +
+
+                $"\n Employee 3 = ${Emp3Pay} " +
+
+                $"\n Employee 4 = ${Emp4Pay}" +
+
+                $"\n Employee 5 = ${Emp5Pay}");
+
+            double TotalPay = Emp1Pay + Emp2Pay + Emp3Pay + Emp4Pay + Emp5Pay;
+            Console.WriteLine($"\n Your total employee pay comes out to ${TotalPay}");
+            Console.ReadLine();
+            Switch();
+
+
+
+        }
+
+
+
+
+
+
+
+        static void Exit()
+        {
+            string Time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.Clear();
+            Console.WriteLine($"                                                                       {Time}\n\n\n\n\n\n\n\n\n                                                 Goodbye!              ");
+            Task.Delay(1000).Wait();
+            System.Environment.Exit(1);
+        }
+    }
+
+}
---
+++ HouseOfCoffee/MyClassLibrary/MyClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+
+    public struct Total
+
+    {
+
+        
+        public static double totalCoffeeSales, totalCoffeeCostOfGoods;
+        public static double totalFoodSales, totalFoodCosts;
+        public double EmployeePay;
+
+        public void CalculateCTotals(int total, double[] CoffeePrice, Sizes sizes, double[] CoffeeCost)
+        {
+            Total.totalCoffeeSales += CoffeePrice[(int)sizes] * total;
+            Total.totalCoffeeCostOfGoods += CoffeeCost[(int)sizes] * total;
+
+        }
+        public void CalculateFTotals(int total, double[] FoodPrice, Food food,double[] FoodCost)
+        {
+            Total.totalFoodSales += FoodPrice[(int)food] * total;
+            Total.totalFoodCosts += FoodCost[(int)food] * total;
+        }
+        public double CalculatePay(int hour, double[] HourPay, Pay pay, double[] Overtime, double[] OTDiff)
+        {
+            if (hour <= 40)
+            {
+                EmployeePay = HourPay[(int)pay] * hour;
+                return EmployeePay;
+            }
+
+            else
+            {
+                EmployeePay = (Overtime[(int)pay] * hour) - (40 * OTDiff[(int)pay]);
+                return EmployeePay;
+
+                //(50 * 18.75) - (40 * 6.25)
+                // hour * Overtime - 40 * OTDiff
+            }
+       
+        }
+
+
+
+
+
+    }
+
+    public class Coffee
+    {
+
+
+
+        public double[] CoffeePrice = { 1.00, 3.00, 5.00 };
+        public double[] FoodPrice = { 5.50, 7.50 };
+        public double[] CoffeeCost = { .10, .15, .20 };
+        public double[] FoodCost = { .95, 1.20 };
+        public double[] HourPay = { 12.50 };
+        public double[] Overtime = { 18.75 };
+        public double[] OTDiff = { 6.25 };
+    }
+
+    public enum Menu { Calculate = 1, Pay, Exit }
+    public enum Sizes { Small, Medium, Large }
+    public enum Food { Egg, Chicken }
+    public enum Pay { hourly }
+}
+
+
+    
+
